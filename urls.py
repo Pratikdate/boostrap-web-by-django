c10af456b3428a5ce00a9e20d0cc96159a02ee54
Pratik date <91735895+Pratikdate@users.noreply.github.com>
2021-12-15 18:00:19 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,29 +1,34 @@
+"""hallo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include(' .urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from app2 import views
-from app2.models import post
+from django.urls import path,include
+from app2 import urls
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path("home",include("app.urls")),
     
-    path("", views.home,
-name="home"),
-    path("/", views.home,           name="home"),
-    path("abouts/",views.about, name="abouts"),
+    #path("about/",include("app.urls")),
     
-    path("services/", views.service, name="services"),
+    #path("contact",include("app.urls")),
     
-    path("blogs/", views.bloghome, name="blogs"),
-    path("blog", views.blog, name="blog"),
-    path("blogs/<str:slug>", views.blogslug, name="blogslug"),
-    path("blog/", views.blog, name="blog"),
-    path("useraccount", views.useraccount, name="useraccount"),
+    #path("images",include("app.urls")),
     
-    path("blog/{{post.slug}}", views.useraccount, name="useraccount"),
+    path("",include("app2.urls")),
+    #path("",include("app.urls")),
     
-    path("login", views.loginuser, name="login"),
-    path("login/", views.loginuser, name="logins"),
-    
-    path("logout", views.userlogout, name="logout"),
-    
-    
+   # path("service",include("app.urls"))
     
 ]
